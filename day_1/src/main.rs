b8e55bebd3b15310e0d279506a21af75ed94bf36
David Skyberg <davidskyberg@gmail.com>
2023-12-02 11:20:33 -0500

    day 1

---
+++ day_1/src/main.rs
@@ -0,0 +1,121 @@
+fn find_first_digit(data: &str) -> Option<(char, usize)> {
+    for (idx, val) in data.chars().enumerate() {
+        if val.is_ascii_digit() {
+            return Some((val, idx));
+        }
+    }
+    None
+}
+
+fn find_last_digit_before(data: &str, before: usize) -> Option<char> {
+    let last = data.len() - before;
+    for (idx, val) in data.chars().rev().enumerate() {
+        if idx >= last {
+            eprintln!("No last digit before first digit");
+            return None;
+        }
+        if val.is_ascii_digit() {
+            return Some(val);
+        }
+    }
+    None
+}
+
+fn make_number(first: char, last: char) -> Result<usize, String> {
+    let val = format!("{}{}", first, last)
+        .parse::<usize>()
+        .map_err(|e| e.to_string())?;
+
+    Ok(val)
+}
+
+fn starts_with(value: &Vec<char>, target: &Vec<char>) -> bool {
+    if target.len() > value.len() {
+        return false;
+    }
+    for idx in 0..target.len() {
+        if value[idx] != target[idx] {
+            return false;
+        }
+    }
+    true
+}
+
+/// Turn number words into digits.  So, 'one' becomes '1', etc.
+/// WARNING!!  'eightwo' should become '82', not '8wo'.
+fn replace_words(in_row: &str, words: &Vec<Vec<char>>) -> Result<String, String> {
+    let mut row_vec = in_row.chars().collect::<Vec<char>>();
+    let mut result = Vec::<char>::new();
+
+    while !row_vec.is_empty() {
+        let mut found = false;
+        for word_idx in 0..words.len() {
+            if starts_with(&row_vec, &words[word_idx]) {
+                let val = char::from_digit(word_idx as u32, 10).unwrap();
+                result.push(val);
+                found = true;
+                break;
+            }
+        }
+        if !found {
+            result.push(row_vec[0]);
+        }
+        if !row_vec.is_empty() {
+            row_vec = row_vec[1..].to_vec();
+        }
+    }
+
+    let row: String = result.iter().collect();
+    Ok(row)
+}
+
+fn part_one() -> Result<(), String> {
+    let data_file = include_str!("../../data/day_1.txt");
+    let data = data_file.split('\n').collect::<Vec<&str>>();
+    let mut total: usize = 0;
+    for row in data {
+        let (first, idx) = find_first_digit(row).ok_or("Failed to find first")?;
+        let last = find_last_digit_before(row, idx).ok_or("Failed to find last")?;
+        let val = make_number(first, last).expect("oops");
+        total += val;
+    }
+    println!("Part one: {}", total);
+    Ok(())
+}
+
+fn part_two() -> Result<(), String> {
+    let words: Vec<Vec<char>> = vec![
+        "zero".chars().collect::<Vec<char>>(),
+        "one".chars().collect::<Vec<char>>(),
+        "two".chars().collect::<Vec<char>>(),
+        "three".chars().collect::<Vec<char>>(),
+        "four".chars().collect::<Vec<char>>(),
+        "five".chars().collect::<Vec<char>>(),
+        "six".chars().collect::<Vec<char>>(),
+        "seven".chars().collect::<Vec<char>>(),
+        "eight".chars().collect::<Vec<char>>(),
+        "nine".chars().collect::<Vec<char>>(),
+    ];
+    let data_file = include_str!("../../data/day_1.txt");
+    let data = data_file.split('\n').collect::<Vec<&str>>();
+    let mut total: usize = 0;
+    for orig in data {
+        let orig = orig.trim();
+        if orig.is_empty() {
+            continue;
+        }
+        let row = replace_words(orig, &words)?;
+        let (first, idx) = find_first_digit(&row).ok_or("Failed to find first")?;
+        let last = find_last_digit_before(&row, idx).ok_or("Failed to find last")?;
+        let val = make_number(first, last).expect("oops");
+        total += val;
+    }
+    println!("Part two: {}", total);
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    part_one()?;
+    part_two()?;
+    Ok(())
+}
