100fbb2f065e505e77e2fff37c7aca451dff8712
dskyberg <davidskyberg@gmail.com>
2023-12-08 08:47:34 -0500

    Day 5 part 1

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,20 +1,22 @@
-#[derive(Debug, Default)]
-struct AlmanacEntry {
-    seed: usize,
-    soil: usize,
-    fertilizer: usize,
-    water: usize,
-    light: usize,
-    temerature: usize,
-    humidity: usize,
-    location: usize,
-}
-
 #[derive(Debug)]
 struct MapEntry {
     destination: usize,
     source: usize,
     range: usize,
+}
+
+impl std::fmt::Display for MapEntry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", &self.destination, &self.source, &self.range)
+    }
+}
+impl MapEntry {
+    fn map_value(&self, value: usize) -> usize {
+        if value >= self.source && value <= self.source + self.range {
+            return self.destination + (value - self.source);
+        }
+        value
+    }
 }
 
 impl TryFrom<&str> for MapEntry {
@@ -46,6 +48,45 @@ impl TryFrom<&str> for MapEntry {
 }
 
 #[derive(Debug, Default)]
+struct AlmanacEntry {
+    seed: usize,
+    soil: usize,
+    fertilizer: usize,
+    water: usize,
+    light: usize,
+    temperature: usize,
+    humidity: usize,
+    location: usize,
+}
+
+impl std::fmt::Display for AlmanacEntry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Seed {}, soil {}, fertilizer {}, water {}, light {}, temerature {}, humidity {}, location {}",
+            self.seed,
+            self.soil,
+            self.fertilizer,
+            self.water,
+            self.light,
+            self.temperature,
+            self.humidity,
+            self.location
+        )
+    }
+}
+
+fn map_maps(value: usize, entries: &Vec<MapEntry>) -> usize {
+    for entry in entries {
+        let tmp_value = entry.map_value(value);
+        if tmp_value != value {
+            return tmp_value;
+        }
+    }
+    value
+}
+
+#[derive(Debug, Default)]
 struct Almanac {
     seeds: Vec<usize>,
     seed_to_soil: Vec<MapEntry>,
@@ -57,8 +98,88 @@ struct Almanac {
     humidity_to_location: Vec<MapEntry>,
 }
 
+impl std::fmt::Display for Almanac {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut result = String::new();
+        result.push_str(&format!("seeds: {}\n", nums_to_str(&self.seeds)));
+
+        result.push_str("\nseed-to-soil\n");
+        for entry in &self.seed_to_soil {
+            result.push_str(&format!("{}\n", entry));
+        }
+
+        result.push_str("\nsoil-to-fertilizer\n");
+        for entry in &self.soil_to_fertilizer {
+            result.push_str(&format!("{}\n", entry));
+        }
+
+        result.push_str("\nfertilizer-to-water\n");
+        for entry in &self.fertilizer_to_water {
+            result.push_str(&format!("{}\n", entry));
+        }
+
+        result.push_str("\nwater-to-light\n");
+        for entry in &self.water_to_light {
+            result.push_str(&format!("{}\n", entry));
+        }
+
+        result.push_str("\nlight-to-temperature\n");
+        for entry in &self.light_to_temperature {
+            result.push_str(&format!("{}\n", entry));
+        }
+
+        result.push_str("\ntemperature-to-humidity\n");
+        for entry in &self.temperature_to_humidity {
+            result.push_str(&format!("{}\n", entry));
+        }
+
+        result.push_str("\nhumidity-to-location\n");
+        for entry in &self.humidity_to_location {
+            result.push_str(&format!("{}\n", entry));
+        }
+
+        write!(f, "{}", result)
+    }
+}
+impl Almanac {
+    fn entry(&self, seed: usize) -> AlmanacEntry {
+        let soil = map_maps(seed, &self.seed_to_soil);
+        let fertilizer = map_maps(soil, &self.soil_to_fertilizer);
+        let water = map_maps(fertilizer, &self.fertilizer_to_water);
+        let light = map_maps(water, &self.water_to_light);
+        let temperature = map_maps(light, &self.light_to_temperature);
+        let humidity = map_maps(temperature, &self.temperature_to_humidity);
+        let location = map_maps(humidity, &self.humidity_to_location);
+
+        AlmanacEntry {
+            seed,
+            soil,
+            fertilizer,
+            water,
+            light,
+            temperature,
+            humidity,
+            location,
+        }
+    }
+
+    fn entries(&self) -> Vec<AlmanacEntry> {
+        let mut entries: Vec<AlmanacEntry> = Vec::new();
+
+        for seed in &self.seeds {
+            entries.push(self.entry(*seed))
+        }
+        entries
+    }
+}
+
 fn part_one(almanac: &Almanac) -> Result<(), String> {
-    println!("Part One:");
+    let entries = almanac.entries();
+    let mut min_loc = entries[0].location;
+    for entry in &entries {
+        min_loc = std::cmp::min(min_loc, entry.location);
+    }
+    println!("Part One: {}", min_loc);
     Ok(())
 }
 fn part_two(_almanac: &Almanac) -> Result<(), String> {
@@ -69,9 +190,15 @@ fn part_two(_almanac: &Almanac) -> Result<(), String> {
 fn main() -> Result<(), String> {
     let input = include_str!("../../data/day_5.txt");
     let almanac = parse_almanac(input)?;
+    // Answer: 331445006
     part_one(&almanac)?;
     part_two(&almanac)?;
     Ok(())
+}
+
+fn nums_to_str(v: &Vec<usize>) -> String {
+    let nums = v.iter().map(|v| v.to_string()).collect::<Vec<String>>();
+    nums.join(", ")
 }
 
 fn parse_almanac(input: &str) -> Result<Almanac, String> {
@@ -94,6 +221,8 @@ fn parse_almanac(input: &str) -> Result<Almanac, String> {
     cursor += 1;
     // Process the seed-to-soil map
     while !lines[cursor].is_empty() {
+        let entry = MapEntry::try_from(lines[cursor])?;
+        almanac.seed_to_soil.push(entry);
         cursor += 1
     }
     cursor += 1;
@@ -161,7 +290,6 @@ fn parse_almanac(input: &str) -> Result<Almanac, String> {
         cursor += 1
     }
 
-    println!("{:?}", &almanac);
     Ok(almanac)
 }
 #[cfg(test)]
@@ -169,9 +297,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_it() {
-        let v = vec![10, 20, 30];
-        let first = v.first().unwrap();
-        println!("{}: {:?}", &first, &v);
+    fn test_maps() {
+        let maps = vec![
+            MapEntry {
+                destination: 50,
+                source: 98,
+                range: 2,
+            },
+            MapEntry {
+                destination: 52,
+                source: 50,
+                range: 48,
+            },
+        ];
+
+        assert_eq!(0, map_maps(0, &maps));
+        assert_eq!(52, map_maps(50, &maps));
+        assert_eq!(53, map_maps(51, &maps));
+        assert_eq!(50, map_maps(98, &maps));
     }
 }
